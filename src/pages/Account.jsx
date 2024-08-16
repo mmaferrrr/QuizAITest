ff8ba04a9234d43927d0a663eb42bb89e03513e1
Joshua Drakeford <JD@Administrators-MacBook-Pro.local>
2024-08-16 18:33:34 -0400

    Developed account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import './AccountPage.css';
+import { HiMiniFire } from "react-icons/hi2";
+import { MdViewList } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
 
-function Account() {
+function AccountPage() {
   return (
-    <div>
-      <h1>Hi maria</h1>
+    <main>
+    <h1>Account</h1>
+    <div className='account-container'>
+      <div className="info-box">
+        <HiMiniFire className='icon' />
+        <h2>Streak</h2>
+        <p>You have a streak of 5 days!</p>
+      </div> 
+      
+      <div className="info-box">
+        <MdViewList className='icon' />
+        <h2>Platinum Quizzes</h2>
+        <ul>
+          <li>golang - intermediate</li>
+          <li>Javascript - beginner</li>
+          <li>AWS - beginner</li>
+        </ul>
+      </div>
+      
+      <div className="info-box">
+        <FaUser className='icon' />
+        <h2>lrnr Level: 2</h2>
+        <p>150/200 xp</p>
+      </div>
     </div>
-  )
+  </main>
+  );
 }
 
-export default Account
+export default AccountPage;
